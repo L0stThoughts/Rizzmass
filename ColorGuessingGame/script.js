322f0c6bd362aeac81b35b99ff03c8c3a13571c8
lost <danielschoolxd@gmail.com>
2024-01-10 19:29:23 +0100

    AARAARARAAAAAAA

--- ColorGuessingGame/script.js
+++ ColorGuessingGame/script.js
@@ -21,11 +21,24 @@ const button1 = document.getElementById('button1').style.background=color;
 const button2 = document.getElementById('button2').style.background=color;
 const button3 = document.getElementById('button3').style.background=color;
 
-button1.addEventListener('click', function(e) {
-    if (e.target.id === "stroke") {
-        
+document.getElementById("button1").addEventListener('click', function(e) {
+    if (e.target && e.target.matches("li")) {
+        document.getElementById("button1").style.opacity = 0;
     }
 });
+
+document.getElementById("button2").addEventListener('click', function(e) {
+    if (e.target && e.target.matches("li")) {
+        document.getElementById("button2").style.opacity = 0;
+    }
+});
+
+document.getElementById("button3").addEventListener('click', function(e) {
+    if (e.target && e.target.matches("li")) {
+        document.getElementById("button3").style.opacity = 0;
+    }
+});
+
 
 
 ul.addEventListener('click', function(e) {
